c55c0cc4fad21603bc297095226a1b3b4aa98e7c
Anurag Kashyap <anuragkashyap815353@gmail.com>
2023-04-27 23:48:11 +0530

    frenzy_flutter_seller

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -81,7 +81,7 @@ class _SplashScreenState extends State<SplashScreen> {
                             style: TextStyle(
                                 fontSize: 25,
                                 fontWeight: FontWeight.w500,
-                                letterSpacing: 0.7),
+                                letterSpacing: 0.6),
                           )),
                     ],
                   ),
